ba5aeeea164f2aed6ec79a6e1bc8ba4f73ef76e7
MFIAtakan <mfi.atakanugur@gmail.com>
2023-11-10 22:43:07 +0000

    Gateway added and configured.

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,29 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers
+{
+    public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+    {
+        private readonly AuctionDbContext _context;
+
+        public AuctionFinishedConsumer(AuctionDbContext context)
+        {
+            this._context = context;
+        }
+        public async Task Consume(ConsumeContext<AuctionFinished> context)
+        {
+            var auction = await _context.Auctions.FindAsync(context.Message.AuctionId);
+
+            if (context.Message.ItemSold)
+            {
+                auction.Winner = context.Message.Winner;
+                auction.SoldAmount = (int)context.Message.Amount;
+            }
+
+            auction.Status = auction.SoldAmount > auction.ReservePrice ? Entities.Status.Finished : Entities.Status.ReserveNotMet;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,3 +1,4 @@
+using AuctionService.Consumers;
 using AuctionService.Data;
 using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -22,6 +23,9 @@ builder.Services.AddMassTransit(x =>
         o.UsePostgres();
         o.UseBusOutbox();
     });
+
+    x.AddConsumersFromNamespaceContaining<AuctionFinishedConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction",false));
 
     x.UsingRabbitMq((context, cfg) =>
     {
